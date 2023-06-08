93c5253f702de355b41dccb8a859cafd7101a27b
MuchamadTaufik <muhamadtaufikm10@gmail.com>
2023-06-09 01:45:53 +0700

    Merubah Menu

--- app/src/main/java/com/e/tubesmobile/screens/Menu.kt
+++ app/src/main/java/com/e/tubesmobile/screens/Menu.kt
@@ -4,9 +4,9 @@ import androidx.annotation.StringRes
 import com.e.tubesmobile.R
 
 enum class Menu(
-        @StringRes val title: Int,
-        val icon: Int,
-        val route: String
+    @StringRes val title: Int,
+    val icon: Int,
+    val route: String
 ) {
     HOME(R.string.home, R.drawable.baseline_home_24, "home"),
     PENGELOLAAN_KOMPUTER(R.string.pengelolaan_komputer, R.drawable.baseline_computer_24, "pengelolaan-komputer"),
@@ -17,12 +17,12 @@ enum class Menu(
         fun getTabFromResource(@StringRes resource: Int) : Menu
         {
             return when (resource) {
-            R.string.home -> HOME
-            R.string.pengelolaan_komputer -> PENGELOLAAN_KOMPUTER
-            R.string.pengelolaan_periferal -> PENGELOLAAN_PERIFERAL
-            R.string.pengelolaan_smarthphone -> PENGELOLAAN_SMARTHPHONE
+                R.string.home -> HOME
+                R.string.pengelolaan_komputer -> PENGELOLAAN_KOMPUTER
+                R.string.pengelolaan_periferal -> PENGELOLAAN_PERIFERAL
+                R.string.pengelolaan_smarthphone -> PENGELOLAAN_SMARTHPHONE
                 else -> SETTING
-        }
+            }
         }
     }
 }
