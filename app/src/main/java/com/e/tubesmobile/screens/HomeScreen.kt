4002c75e3eee5f5756c9f35f64074b6d58613a3e
MuchamadTaufik <muhamadtaufikm10@gmail.com>
2023-06-08 23:02:50 +0700

    Push

---
+++ app/src/main/java/com/e/tubesmobile/screens/HomeScreen.kt
@@ -0,0 +1,133 @@
+package com.e.tubesmobile.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.SnackbarHostState
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import com.e.tubesmobile.R
+
+@Composable
+fun HomeScreen(navController: NavHostController, snackbarHostState: SnackbarHostState, modifier: Modifier = Modifier) {
+    val menus = listOf(
+        Menu.HOME,
+        Menu.PENGELOLAAN_KOMPUTER,
+        Menu.PENGELOLAAN_PERIFERAL,
+        Menu.PENGELOLAAN_SMARTHPHONE,
+        Menu.SETTING)
+    val listState = rememberLazyListState()
+
+    Column(
+        modifier = Modifier.run {
+            fillMaxSize()
+                .background(Color.Black)
+        },
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Image(modifier = Modifier
+            .size(200.dp),
+            painter = painterResource(
+                id = R.drawable.logo),
+            contentDescription = "Halaman Home"
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text = "Welcome",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Medium,
+            color = Color.White
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(
+            modifier = Modifier.padding(end = 78.dp, start = 78.dp),
+            text = "Lagi butuh Komputer, Periferal, dan Smarthphone, Disini adalah tempat yang tepat",
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Light,
+            color = Color.White,
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+        Button(
+            onClick = {
+                navController.navigate("pengelolaan-komputer")
+            },
+            modifier = Modifier
+                .height(45.dp)
+                .width(200.dp),
+            colors = ButtonDefaults.buttonColors(Color.Red),
+            shape = RoundedCornerShape(8.dp)
+        ) {
+            Text(
+                text = "Komputer",
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Medium,
+                color = Color.White
+            )
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(
+            onClick = { navController.navigate("pengelolaan-periferal") },
+            modifier = Modifier
+                .height(45.dp)
+                .width(200.dp),
+            colors = ButtonDefaults.buttonColors(Color.Red),
+            shape = RoundedCornerShape(8.dp)
+        ) {
+            Text(
+                text = "Periferal",
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Medium,
+                color = Color.White
+            )
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(
+            onClick = { navController.navigate("pengelolaan-smarthphone") },
+            modifier = Modifier
+                .height(45.dp)
+                .width(200.dp),
+            colors = ButtonDefaults.buttonColors(Color.Red),
+            shape = RoundedCornerShape(8.dp)
+        ) {
+            Text(
+                text = "Smarthphone",
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Medium,
+                color = Color.White
+            )
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(
+            onClick = {
+                navController.navigate("setting")
+            },
+            modifier = Modifier
+                .height(45.dp)
+                .width(200.dp),
+            colors = ButtonDefaults.buttonColors(Color.Red),
+            shape = RoundedCornerShape(8.dp)
+        ) {
+            Text(
+                text = "Pengaturan",
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Medium,
+                color = Color.White
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/e/tubesmobile/screens/Menu.java
@@ -0,0 +1,28 @@
+package com.e.tubesmobile.screens
+
+import androidx.annotation.StringRes
+import com.e.tubesmobile.R
+
+enum class Menu(
+        @StringRes val title: Int,
+        val icon: Int,
+        val route: String
+) {
+    HOME(R.string.home, R.drawable.baseline_home_24, "home"),
+    PENGELOLAAN_KOMPUTER(R.string.pengelolaan_komputer, R.drawable.baseline_computer_24, "pengelolaan-komputer"),
+    PENGELOLAAN_PERIFERAL(R.string.pengelolaan_periferal, R.drawable.baseline_mouse_24, "pengelolaan-periferal"),
+    PENGELOLAAN_SMARTHPHONE(R.string.pengelolaan_smarthphone, R.drawable.baseline_phone_android_24, "pengelolaan-smarthphone"),
+    SETTING(R.string.setting, R.drawable.baseline_settings_24, "setting");
+    companion object {
+        fun getTabFromResource(@StringRes resource: Int) : Menu
+        {
+            return when (resource) {
+            R.string.home -> HOME
+            R.string.pengelolaan_komputer -> PENGELOLAAN_KOMPUTER
+            R.string.pengelolaan_periferal -> PENGELOLAAN_PERIFERAL
+            R.string.pengelolaan_smarthphone -> PENGELOLAAN_SMARTHPHONE
+                else -> SETTING
+        }
+        }
+    }
+}
